50b6ae5ba7b77de8ce320b80cedcef2d077a0c5e
Cipick23 <133102658+Cipick23@users.noreply.github.com>
2023-11-03 16:30:55 +0200

    deleted src file

--- src/js/task-05.js
+++
@@ -1,7 +0,0 @@
-const nameInput = document.getElementById('name-input');
-const nameOutput = document.getElementById('name-output');
-
-nameInput.addEventListener('input', () => {
-  const newName = nameInput.value || 'Anonymous';
-  nameOutput.textContent = newName;
-});
